046e20833a0962545a79131fd0792249faedc8b2
Matěj Novotný <xnovot19@fi.muni.cz>
2022-06-18 00:33:19 +0200

    Remove typo

--- src/hooks/usePageTitle.ts
+++ src/hooks/usePageTitle.ts
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 
 const usePageTitle = (title: string) => {
 	useEffect(() => {
-		document.title = `${title} | GithubFinder}`;
+		document.title = `${title} | GithubFinder`;
 	}, [title]);
 };
 
